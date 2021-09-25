1cde5fbe33d9e56de0a04ef0d1cfc97b84052a70
Amey Ayare <amey.ayare@netcore.co.in>
2021-09-25 21:31:57 +0530

    code added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceComponent } from './service/service.component';
+import { ServicedetailsComponent } from './servicedetails/servicedetails.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutusComponent},
+  {path:"service",component:ServiceComponent},
+  {path:"servicedetails",component:ServicedetailsComponent},
+  {path:"contact",component:ContactusComponent},
+  {path:"",component:HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServiceComponent } from './service/service.component';
+import { ServicedetailsComponent } from './servicedetails/servicedetails.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ServiceComponent,
+    ServicedetailsComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
